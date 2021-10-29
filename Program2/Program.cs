a5fa6ada38382b60d80aec6807b4ffcbc68ef353
Dasha <bufebaa@gmail.com>
2021-10-29 19:16:10 +0300

    Changes in classes for comfot

--- Program2/Program.cs
+++ Program2/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace program1
+namespace Program2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Code1.Task1();
+            Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ program1/Code1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Code1
+    {
+        public static void Task1()
+        {
+            const string MyName = "Dasha";
+            byte MyAge = 18;
+            bool HaveIApet = true;
+            double MyShoeSize = 38;
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? : " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.WriteLine("\u0003");
+            Console.ReadKey();
+        }
+    }
+}
