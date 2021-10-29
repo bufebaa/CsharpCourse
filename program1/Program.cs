40e497711f1fb06ce782c3d65d540a1eca990f44
Dasha <bufebaa@gmail.com>
2021-10-29 19:01:24 +0300

    Add some changes

--- program1/Program.cs
+++ program1/Program.cs
@@ -6,9 +6,15 @@ namespace program1
     {
         static void Main(string[] args)
         {
-            string MyName = "Dasha";
-            Console.WriteLine(MyName);
-
+            const string MyName = "Dasha";
+            byte MyAge = 18;
+            bool HaveIApet = true;
+            double MyShoeSize = 38;
+            Console.WriteLine("My name is "+ MyName);
+            Console.WriteLine("My age is "+ MyAge);
+            Console.WriteLine("Do I have a pet? : "+ HaveIApet);
+            Console.WriteLine("My shoe size is "+ MyShoeSize);
+            Console.WriteLine("\u0003");
             Console.ReadKey();
         }
     }
