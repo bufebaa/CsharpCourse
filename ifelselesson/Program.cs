be5c0e1d1ca95fdb65b1e373794bd3ca68ba4228
Dasha <bufebaa@gmail.com>
2021-11-08 17:26:18 +0200

    New modue programms

---
+++ ifelselesson/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ifelselesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1.Tone();
+            //Task1.Ttwo();
+            //Task1.Tthree();
+            //Task1.Tfour();
+            //Task1.Tsix();
+            int k = 0;
+            while (k < 3)
+            {
+                (string Name, string LName, string Login, int LoginLength, bool Haspet, double Age, string[] favcolors) User;
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+                Console.WriteLine("Введите фамилию");
+                User.LName = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var result = Console.ReadLine();
+                if (result == "Yes")
+                    User.Haspet = true;
+                else
+                    User.Haspet = false;
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(Console.ReadLine());
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+                Console.WriteLine("\n\n");
+                k++;
+            }
+
+        }
+    }
+}
---
+++ ifelselesson/Task1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelselesson
+{
+    class Task1
+    {
+        public static void Tone()
+        {
+            string name;
+            
+            Console.WriteLine("Enter your name: ");
+            name = "Dasha";
+            Console.WriteLine("Ваше имя по буквам");
+            foreach(var i in name)
+            {
+                Console.Write(i+" ");
+            }
+        }
+
+        public static void Ttwo()
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int count = 0;
+            foreach(var i in arr)
+            {
+                count += i;
+            }
+            Console.WriteLine("Sum: "+count);
+        }
+
+        public static void Tthree()
+        {
+            int[][] array = new int[3][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+            foreach(var num in array)
+            {
+                foreach(var item in num)
+                {
+                    Console.WriteLine(item+" ");
+                }
+            }
+        }
+
+        public static void Tfour()
+        {
+            int[] arr = { 1, -9, 4, 6, 8, -23, -8, 4 };
+            int count = 0;
+            foreach(var i in arr)
+            {
+                if (i < 0)
+                    count += 1;
+            }
+            Console.WriteLine("Колличество отрицательных чисел: "+count);
+        }
+
+        public static void Tsix()
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int count = 0;
+            foreach (var i in arr)
+            {
+                if (i > 0)
+                    count += 1;
+            }
+            Console.WriteLine("Колличество положительных чисел: " + count);
+
+        }
+
+        public static void Tseven()
+        {
+            
+        }
+    }
+}
